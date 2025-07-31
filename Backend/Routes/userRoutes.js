00caa20fd8791505b111c834f14286a1c882ebc4
realhariom <digitalwithhariom@gmail.com>
2025-08-01 01:30:53 +0530

    Update userRoutes.js

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,5 +1,6 @@
   import express from "express";
   import { loginUser, registerUser, userLogout, userProfile } from "../Controllers/authControllers.js";
+import { authenticateUser } from "../Middlewares/validateToken.js";
 
 
 const router = express.Router();
@@ -9,8 +10,8 @@ const router = express.Router();
   // @access public
   
 router.post("/register", registerUser)
-router.get("/profile",userProfile)
-router.post("/logout",userLogout)
+router.get("/userProfile",authenticateUser ,userProfile)
+router.post("/logout",authenticateUser,userLogout)
 router.post("/login",loginUser)
 
 export default router;
