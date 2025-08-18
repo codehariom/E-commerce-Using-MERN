f2db7115a7dfbc965b6b8549e024ee32f0890309
realhariom <digitalwithhariom@gmail.com>
2025-08-18 17:51:44 +0530

    redux Store

---
+++ Frontend/src/redux/store.js
@@ -0,0 +1,22 @@
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from "./authSlice.js"
+import productReducer from "./productSlice.js"
+import cartReducer from "./cartSlice.js"
+import checkoutReducer from "./checkoutSlice.js"
+import orderReducer from "./orderSlice.js"
+import adminReducer from "./adminSlice.js"
+import adminProductsReducer from "./adminProductSlice.js"
+import adminOrderReducer from "./adminOrderSlice.js"
+
+export const store = configureStore({
+  reducer: {
+    auth:authReducer,
+    products:productReducer,
+    carts:cartReducer,
+    checkout:checkoutReducer,
+    order:orderReducer,
+    admin:adminReducer,
+    adminProducts:adminProductsReducer,
+    adminOrder:adminOrderReducer
+  },
+})
