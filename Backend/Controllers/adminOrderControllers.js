ca20171b3e3b5b8baec2a6ec8d3ba7844dccd059
realhariom <digitalwithhariom@gmail.com>
2025-09-29 00:21:07 +0530

    fix the bug

--- Backend/Controllers/adminOrderControllers.js
+++ Backend/Controllers/adminOrderControllers.js
@@ -1,47 +1,54 @@
 import expressAsyncHandler from "express-async-handler";
 import { Order } from "../Models/orderModel.js";
 
-
-export const allOrders = expressAsyncHandler (async(req, res)=>{
+export const allOrders = expressAsyncHandler(async (req, res) => {
     try {
-        const orders = await Order.find({}).populate("user","name","email")
-        res.status(200).json(orders)
+        const orders = await Order.find({}).populate("user", "name");
+        const totalOrders = orders.length;
+        const totalSales = orders.reduce(
+            (acc, order) => acc + order.totalPrice,
+            0
+        );
+        res.status(200).json(orders, totalOrders, totalSales);
     } catch (error) {
-        console.error(error)
-        res.status(500).json({message:"Server Error"})
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
     }
-})
+});
 
-
-export const orderUpdate = expressAsyncHandler(async(req, res)=>{
-    try {
-        const order = await Order.findById(req.params.id)
-        if(order){
-            order.status = req.body.status || order.status
-            order.isDelivered = req.body.status==="Delivered" ? true :order.isDelivered
-            order.deliveredAt = req.body.status==="Delivered" ? Date.now() :order.deliveredAt
-
-            const updatedOrder = await order.save()
-
-            res.status(200).json(updatedOrder)
-        }
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({message:"Server Error"})
-    }
-})
-
-export const deleteOrder = expressAsyncHandler(async(req, res)=>{
+export const orderUpdate = expressAsyncHandler(async (req, res) => {
     try {
         const order = await Order.findById(req.params.id);
-        if(order){
-            await Order.deleteOne();
-            res.json({message:"Order Removed"})
-        }else{
-            res.status(404).json({message:"Order not found"})
+        if (order) {
+            order.status = req.body.status || order.status;
+            order.isDelivered =
+                req.body.status === "Delivered" ? true : order.isDelivered;
+            order.deliveredAt =
+                req.body.status === "Delivered"
+                    ? Date.now()
+                    : order.deliveredAt;
+
+            const updatedOrder = await order.save();
+
+            res.status(200).json(updatedOrder);
         }
     } catch (error) {
-        console.error(error)
-        res.status(500).json({message:"Server Error"})
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
     }
-})
+});
+
+export const deleteOrder = expressAsyncHandler(async (req, res) => {
+    try {
+        const order = await Order.findById(req.params.id);
+        if (order) {
+            await Order.deleteOne();
+            res.json({ message: "Order Removed" });
+        } else {
+            res.status(404).json({ message: "Order not found" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
--- Backend/Middlewares/adminAuth.js
+++ Backend/Middlewares/adminAuth.js
@@ -1,7 +1,7 @@
 import expressAsyncHandler from "express-async-handler";
 
 export const admin= expressAsyncHandler(async(req,res,next)=>{
-    if(req.user && req.user.role==="Admin"){
+    if(req.user && req.user.role==="admin"){
         next();
     }
     else{
